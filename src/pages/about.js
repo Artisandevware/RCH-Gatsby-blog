f8d784afa9777bf34b5f4845b614b575623886da
Russell Hrycko <Woodhead73@gmail.com>
2020-10-03 16:14:06 -0700

    added about and new home pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { Link } from "gatsby";
 import Header from "../components/header";
 
 export default function About() {
 	return (
 		<div style={{ color: "teal" }}>
+			<Link to="/contact/">Contact</Link>
+			<Link to="/about/">About</Link>
+			<Link to="/blog/">Blog</Link>
 			<Header headerText="About Gatsby" />
 			<Header headerText="It's pretty cool" />
 			<p>Such wow. Very React.</p>
+
+			
 		</div>
 	);
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,8 @@ export default function Home() {
 	return (
 		<div style={{ color: "white" }}>
 			<Link to="/contact/">Contact</Link>
+			<Link to="/about/">About</Link>
+			<Link to="/blog/">Blog</Link>
 			<Header headerText="Hello Gatsby!" />
 			<p>What a world.</p>
 			<img src="https://source.unsplash.com/random/400x200" alt="" />
