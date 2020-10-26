f62c5a992e543b1181d63d965a028a014e52998f
Russell Hrycko <Woodhead73@gmail.com>
2020-10-25 20:52:06 -0700

    fix to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,82 +1,17 @@
-/*import React from "react";
+
+import React from "react";
 import { Link } from "gatsby";
 import Header from "../components/header";
-import Layout from "../components/layout";
 
 export default function Home() {
 	return (
-		<Layout location={location} title={siteTitle}>
-			<div style={{ color: "white" }}>
-				<Link to="/contact/">Contact</Link>
-				<Link to="/about/">About</Link>
-				<Link to="/blog/">Blog</Link>
-				<Header headerText="Hello Gatsby!" />
-				<p>What a world.</p>
-				<img src="https://source.unsplash.com/random/400x200" alt="" />
-			</div>
-		</Layout>
+		<div style={{ color: "white" }}>
+			<Link to="/contact/">Contact</Link>
+			<Link to="/about/">About</Link>
+			<Link to="/blog/">Blog</Link>
+			<Header headerText="Hello Gatsby!" />
+			<p>What a world.</p>
+			<img src="https://source.unsplash.com/random/400x200" alt="" />
+		</div>
 	);
-}
-*/
-import React from "react";
-import Helmet from "react-helmet";
-import { graphql } from "gatsby";
-import Layout from "../components/layout";
-import PostLink from "../components/post-link";
-import HeroHeader from "../components/heroHeader";
-
-
-const IndexPage = ({
-	data: {
-		site,
-		allMarkdownRemark: { edges },
-	},
-}) => {
-
-	const Posts = edges
-		.filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
-		.map(edge => <PostLink key={edge.node.id} post={edge.node} />);
-
-	return (
-		<Layout>
-			<Helmet>
-				<title>{site.siteMetadata.title}</title>
-				<meta name="description" content={site.siteMetadata.description} />
-        //{!site.siteMetadata.w3l_dom_key ? null : <meta name="w3l-domain-verification" content={site.siteMetadata.w3l_dom_key} />}
-			</Helmet>
-			<HeroHeader/>
-			<h2>Some of my work &darr;</h2>
-			<div className="grids">
-				{Posts}
-			</div>
-		</Layout>
-	);
-};
-
-export default IndexPage;
-export const pageQuery = graphql`
-  query indexPageQuery {
-    site {
-      siteMetadata {
-        title
-        description 
-        # w3l_dom_key
-        
-      }
-    }
-    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
-      edges {
-        node {
-          id
-          excerpt(pruneLength: 250)
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            path
-            title
-            thumbnail
-          }
-        }
-      }
-    }
-  }
-`;
+}	
